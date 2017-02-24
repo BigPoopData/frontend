7490e1fad6b7361c0842612f6e9982cdc135b5af
funiel <d.wetzelhuetter@gmail.com>
2017-02-24 20:23:31 +0100

    Added onclick-event to graphs

--- js/getdata.js
+++ js/getdata.js
@@ -1,9 +1,12 @@
-var dataset = [];
-var timestampsObject = [];
-var averagesObject = [];
-var datasetObject = [];
+//Initialize Variables
+var averagesPerDayTimestamps = [];
+var averagesPerMonthTimestamps = [];
+var averagesPerDayObject = [];
+var averagesPerMonthObject = [];
+var averagesPerMonthMinutesObject = [];
+var averagesPerDayMinutesObject = [];
 var currentstatus;
-var waterusage = 2523;
+var waterusage = 0;
 var closedcolor = 'rgba(231, 76, 60, ';
 var opencolor = 'rgba(46, 204, 113, ';
 var currentcolor;
@@ -11,12 +14,13 @@ var currentcolorlessopacity;
 var alphafull = '1.0)';
 var alphadown = '0.3)';
 var datetimestamp = new Date();
+var graphcolor;
 
 
-
+//connect to websocket
 var getData = new WebSocket("wss://metaklo.nico-rameder.at:8000/ws");
 
-
+//send setup message
 this.send = function(message, callback) {
     this.waitForConnection(function() {
         getData.send(message);
@@ -26,6 +30,7 @@ this.send = function(message, callback) {
     }, 1000);
 };
 
+//wait until response
 this.waitForConnection = function(callback, interval) {
     if (getData.readyState === 1) {
         callback();
@@ -37,35 +42,45 @@ this.waitForConnection = function(callback, interval) {
     }
 };
 
+//callback of response
 this.send("setup", function() {
     console.log('server is up');
 });
 
+//executes on message from ws
 getData.onmessage = function(msg) {
     result = JSON.parse(msg.data);
     switch (result.name) {
         case "FullObject":
             dataset = result;
-            datasetObject = dataset.averageClosedDurationPerDay;
-            //if (averagesObject.length < 1) {
-            for (var j = 0; j < datasetObject.length; j++) {
-                timestampsObject.push(datasetObject[j].timestamp);
-                averagesObject.push(Math.floor((datasetObject[j].average / 60)* 100) / 100);
+            averagesPerDayObject = result.averageClosedDurationPerDay;
+            averagesPerMonthObject = result.averageClosedDurationPerMonth;
+            for (var j = 0; j < averagesPerMonthObject.length; j++) {
+                averagesPerMonthTimestamps.push(averagesPerMonthObject[j].timestamp);
+                averagesPerMonthMinutesObject.push(Math.floor((averagesPerMonthObject[j].average / 60) * 100) / 100);
             }
-            //}
-            currentstatus = dataset.lastEvent.open;
+            for (var k = 0; k < averagesPerDayObject.length; k++) {
+                averagesPerDayTimestamps.push(averagesPerDayObject[k].timestamp);
+                averagesPerDayMinutesObject.push(Math.floor((averagesPerDayObject[k].average / 60) * 100) / 100);
+            }
+
+
+            currentstatus = result.lastEvent.open;
             var daterightnow = new Date();
-            datetimestamp = Date.parse(dataset.lastEvent.timestamp);
+            datetimestamp = Date.parse(result.lastEvent.timestamp);
             var dateinterval = daterightnow - datetimestamp;
-            var timedurationelapsed = Math.floor(dateinterval/1000);
+            var timedurationelapsed = Math.floor(dateinterval / 1000);
             break;
 
         case "sitzklo":
             currentstatus = JSON.parse(msg.data).open;
-            console.log(msg.data);
             timedurationelapsed = 0;
+            myNewChart.destroy();
+
             break;
     }
+
+    waterusage = result.totalIntervals * 9;
 
     setTimerDurationElapsed(timedurationelapsed);
 
@@ -77,17 +92,19 @@ getData.onmessage = function(msg) {
             currentcolorlessopacity = opencolor + alphadown;
 
             break;
+
         case "false":
             $('#status').text('occupied');
             $('.statuscolor').css("background-color", closedcolor + alphafull);
             currentcolor = closedcolor + alphafull;
             currentcolorlessopacity = closedcolor + alphadown;
     }
-    var graphcolor = currentcolorlessopacity;
+
+    graphcolor = currentcolorlessopacity;
 
 
-    drawgraph1(averagesObject, timestampsObject, graphcolor);
-    drawgraph2(averagesObject, timestampsObject, graphcolor);
+    drawgraph2(averagesPerDayMinutesObject, averagesPerDayTimestamps, graphcolor);
+    drawgraph1(averagesPerMonthMinutesObject, averagesPerMonthTimestamps, graphcolor);
 
     $(".se-pre-con").fadeOut("slow");
     $('#main-content').fadeIn("slow");
--- js/makegraph.js
+++ js/makegraph.js
@@ -4,35 +4,73 @@ function drawgraph1(array, timestamp, barchartcolor) {
         labels: timestamp,
         scaleShowVerticalLines: true,
         datasets: [{
-        //     labels: timestamp,
-        //     scaleShowVerticalLines: true,
-        //     type: 'line',
-        //     label: 'Average Time in Minutes',
-        //     data: array,
-        //     showLine: true,
-        //     borderColor: currentcolor,
-        //     borderWidth: 1,
-        //     tension: 0.1,
-        //
-        // }, {
+            //     labels: timestamp,
+            //     scaleShowVerticalLines: true,
+            //     type: 'line',
+            //     label: 'Average Time in Minutes',
+            //     data: array,
+            //     showLine: true,
+            //     borderColor: currentcolor,
+            //     borderWidth: 1,
+            //     tension: 0.1,
+            //
+            // }, {
             labels: timestamp,
-            scaleShowVerticalLines: true,
-
+            scaleShowVerticalLines: false,
             type: 'bar',
             label: 'Bar Component',
             data: array,
             backgroundColor: barchartcolor,
+            hoverBackgroundColor: currentcolor
         }]
 
     };
-
     Chart.defaults.global.legend.display = false;
 
     var ctx = document.getElementById("myChart");
-    var myNewChart = new Chart(ctx, {
+    myNewChart = new Chart(ctx, {
         type: "bar",
         data: lineChartData,
         options: {
+            tooltips: {
+                mode: 'label',
+
+                custom: function(tooltip) {
+                    tooltip.caretSize = 10,
+                        tooltip.backgroundColor = '#000';
+                    cornerRadius = 100;
+                },
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        var value = data.datasets[0].data[tooltipItem.index];
+                        var label = data.labels[tooltipItem.index];
+                        return value + " Minutes";
+                    },
+                    afterLabel: function(tooltipItem, data) {
+                        if (tooltipItem.index === 0) {
+                            return "";
+                        } else if (tooltipItem.index === 1) {
+                            return "";
+                        } else {
+                            return "";
+                        }
+                    }
+                }
+            },
+            animation: {
+                easing: "easeInOutBounce" // tooltip animation
+            },
+
+            onClick: function handleClick(evt) {
+                var activeElement = myNewChart.getElementAtEvent(evt);
+                var activeLabel = activeElement[0]._chart.config.data.labels[activeElement[0]._index];
+                var activeElementValue = activeElement[0]._chart.config.data.datasets[activeElement[0]._datasetIndex].data[activeElement[0]._index];
+                console.log(activeLabel);
+                console.log(activeElementValue);
+                myNewChart.destroy();
+                customgraph(averagesPerDayMinutesObject, averagesPerDayTimestamps, graphcolor);
+                // drawgraph1(array, timestamp, barchartcolor);
+            },
 
             scales: {
                 xAxes: [{
@@ -64,25 +102,29 @@ function drawgraph1(array, timestamp, barchartcolor) {
                 display: false,
             },
         }
-
     });
-
 }
 
 function drawgraph2(array, timestamp, barchartcolor) {
+    console.log(barchartcolor);
     var lineChartData = {
         labels: timestamp,
         scaleShowVerticalLines: true,
         datasets: [{
-            // type: 'line',
-            // label: 'Average Time in Minutes',
-            // data: array,
-            // showLine: false,
-            // borderColor: currentcolor,
-            // borderWidth: 1,
-            // tension: 0.1,
+            //     labels: timestamp,
+            //     scaleShowVerticalLines: true,
+            //     type: 'line',
+            //     label: 'Average Time in Minutes',
+            //     data: array,
+            //     showLine: true,
+            //     borderColor: currentcolor,
+            //     borderWidth: 1,
+            //     tension: 0.1,
             //
             // }, {
+            labels: timestamp,
+            scaleShowVerticalLines: true,
+
             type: 'bar',
             label: 'Bar Component',
             data: array,
@@ -94,25 +136,69 @@ function drawgraph2(array, timestamp, barchartcolor) {
     Chart.defaults.global.legend.display = false;
 
     var ctx = document.getElementById("myChart2");
-    var myNewChart = new Chart(ctx, {
+    var myNewChart2 = new Chart(ctx, {
         type: "bar",
         data: lineChartData,
         options: {
-
             scales: {
                 xAxes: [{
-                    //     afterTickToLabelConversion: function(data){
-                    //    var xLabels = data.ticks;
-                    //    xLabels.forEach(function (labels, i) {
-                    //        if (i % 2 == 1){
-                    //            xLabels[i] = '';
-                    //        }
-                    //    });},
-                    // ticks: {
-                    //     autoSkip: true,
-                    //     maxTicksLimit: 20
-                    // },
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false,
+            },
+        }
 
+    });
+}
+
+function customgraph(array, timestamp, barchartcolor) {
+    console.log(barchartcolor);
+    var lineChartData = {
+        labels: timestamp,
+        scaleShowVerticalLines: true,
+        datasets: [{
+            //     labels: timestamp,
+            //     scaleShowVerticalLines: true,
+            //     type: 'line',
+            //     label: 'Average Time in Minutes',
+            //     data: array,
+            //     showLine: true,
+            //     borderColor: currentcolor,
+            //     borderWidth: 1,
+            //     tension: 0.1,
+            //
+            // }, {
+            labels: timestamp,
+            scaleShowVerticalLines: true,
+
+            type: 'bar',
+            label: 'Bar Component',
+            data: array,
+            backgroundColor: barchartcolor,
+        }]
+
+    };
+
+    Chart.defaults.global.legend.display = false;
+
+    var ctx = document.getElementById("myChart");
+    var myNewChart2 = new Chart(ctx, {
+        type: "bar",
+        data: lineChartData,
+        options: {
+            scales: {
+                xAxes: [{
                     gridLines: {
                         display: false
                     }
